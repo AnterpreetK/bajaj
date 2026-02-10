d8eda2b512759bc3ab646fe0b2e0271cb1a6adda
AnterpreetK <kanterpreet09@gmail.com>
2026-02-10 12:30:25 +0530

    Fix : use dynamic PORT for Render deployment

--- index.js
+++ index.js
@@ -115,6 +115,8 @@ app.post("/bfhl", async (req, res) => {
 });
 
 /* ---------------- START SERVER ---------------- */
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
